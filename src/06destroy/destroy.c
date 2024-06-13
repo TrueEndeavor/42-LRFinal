29cbccb03e1232cc216309936342d04d9574731c
lannur-s <lathaannur@gmail.com>
2024-06-13 18:29:18 +0200

    norm pending

--- src/06destroy/destroy.c
+++ src/06destroy/destroy.c
@@ -6,62 +6,11 @@
 /*   By: lannur-s <lannur-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 11:51:43 by lannur-s          #+#    #+#             */
-/*   Updated: 2024/06/10 13:14:56 by lannur-s         ###   ########.fr       */
+/*   Updated: 2024/06/11 10:32:07 by lannur-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
-
-void	free_dup_map(t_data *data)
-{
-	int	i;
-
-	i = 0;
-	while (data->dup_map[i])
-	{
-		free(data->dup_map[i]);
-		i++;
-	}
-	free(data->dup_map);
-	data->dup_map = NULL;
-}
-
-void	free_world_map(t_data *data)
-{
-	int	i;
-
-	i = 0;
-	while (i < data->map_height)
-	{
-		if (data->world_map[i])
-			free(data->world_map[i]);
-		i++;
-	}
-	if (data->world_map)
-		free(data->world_map);
-}
-
-void	free_maps(t_data *data)
-{
-	t_list	*current;
-	t_list	*next;
-
-	if (data->world_map)
-		free_world_map(data);
-	if (data->dup_map)
-		free_dup_map(data);
-	if (data->map)
-	{
-		current = data->map;
-		while (current)
-		{
-			free(current->content);
-			next = current->next;
-			free(current);
-			current = next;
-		}
-	}
-}
 
 void	free_textures(t_data *data)
 {
