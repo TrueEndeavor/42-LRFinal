b02778a8d9a1b70c4e711c9983542d3f6ea8d2d4
lannur-s <lathaannur@gmail.com>
2024-06-17 13:01:04 +0200

    [fix] player direction adjustment

--- src/04mapvalidation/player_direction.c
+++ src/04mapvalidation/player_direction.c
@@ -6,7 +6,7 @@
 /*   By: lannur-s <lannur-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/08 20:08:17 by lannur-s          #+#    #+#             */
-/*   Updated: 2024/06/08 20:10:21 by lannur-s         ###   ########.fr       */
+/*   Updated: 2024/06/17 12:37:59 by lannur-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,17 @@ void	set_player_direction_north(t_game *game)
 void	set_player_direction_south(t_game *game)
 {
 	game->dir_x = 1;
-	game->dir_y = -1;
+	game->dir_y = 0;
 	game->plane_x = 0;
 	game->plane_y = 0.66;
 }
 
 void	set_player_direction_east(t_game *game)
 {
-	game->dir_x = -1;
+	game->dir_x = 0;
 	game->dir_y = 1;
 	game->plane_x = 0.66;
-	game->plane_y = 0.66;
+	game->plane_y = 0;
 }
 
 void	set_player_direction_west(t_game *game)
